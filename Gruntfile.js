0860dc40fd21f2c6ad6a587d1428b5d91024e314
Ryan <rburgess@evernote.com>
2014-02-23 16:04:44 -0800

    use libsass

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,8 @@ module.exports = function(grunt) {
 		    }
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	//grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
