3d9aef25ebcbc99711254549833d449638fe0eea
Ryan <rburgess@evernote.com>
2014-02-23 19:04:58 -0800

    add grunt bump to package

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,20 @@ module.exports = function(grunt) {
 		      }]
 		    }
 		},
+		bump: {
+		  options: {
+		  	files: ['package.json', 'bower.json'],
+		    updateConfigs: ["pkg"],
+		    commit: true,
+		    commitMessage: 'Release v%VERSION%',
+		    commitFiles: ['package.json'], // '-a' for all files
+		    createTag: true,
+		    tagName: 'v%VERSION%',
+		    tagMessage: 'Version %VERSION%',
+        	commitFiles: ["-a"],
+        	push: false
+		  }
+		},
 		watch: {
 			css: {
 				files: 'source/**/*.scss',
@@ -83,5 +97,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-newer');
+	grunt.loadNpmTasks('grunt-bump');
 	grunt.registerTask('default',['watch']);
 }
