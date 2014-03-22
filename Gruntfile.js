20a49738881255150b75d2d1e53be99b1d9814e0
Ryan <rburgess@evernote.com>
2014-03-21 21:22:14 -0700

    clean up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,18 +43,18 @@ module.exports = function(grunt) {
             }
         },
         bump: {
-          options: {
-            files: ['package.json', 'bower.json'],
-            updateConfigs: ["pkg"],
-            commit: true,
-            commitMessage: 'Release v%VERSION%',
-            commitFiles: ['package.json'], // '-a' for all files
-            createTag: true,
-            tagName: 'v%VERSION%',
-            tagMessage: 'Version %VERSION%',
-            commitFiles: ["-a"],
-            push: false
-          }
+            options: {
+                files: ['package.json', 'bower.json'],
+                updateConfigs: ["pkg"],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                commitFiles: ["-a"],
+                push: false
+            }
         },
         watch: {
             css: {
